80960672e344a3ca762836eaf1919ee4232fc200
Phil Crumm <pcrumm@p3net.net>
2014-01-01 14:08:32 -0800

    Embarassing typo correction

--- www/add.php
+++ www/add.php
@@ -5,6 +5,6 @@ if ( ! preg_match( '/^[A-Za-z0-9\.\-\_]+$/', $username ) )
 	die( 'failure' );
 
 require '../snapchat.php';
-SnaphchatUtil::add_friend( $username );
+SnapchatUtil::add_friend( $username );
 
 die( 'success' );
