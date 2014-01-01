abb6ddfd93323e39bda8c6040769ef2908680aac
Phil Crumm <pcrumm@p3net.net>
2014-01-01 14:29:58 -0800

    Use full file paths to allow this script to run via cron

--- send_corgi_photo.php
+++ send_corgi_photo.php
@@ -1,6 +1,6 @@
 <?php
-require 'snapchat.php';
-require 'reddit/corgi.php';
+require_once realpath( dirname( __FILE__ ) . 'snapchat.php' );
+require_once realpath( dirname( __FILE__ ) . 'reddit/corgi.php' );
 
 $corgi = file_get_contents( Corgi::fetch_corgi() ) or die();
 SnapchatUtil::send_photo( $corgi, SnapchatUtil::get_friends_list() );
