743fa3dd1eed19f2b00966ac275b8d9858f3f512
Phil Crumm <pcrumm@p3net.net>
2014-01-01 14:01:55 -0800

    Correct case

--- send_corgi_photo.php
+++ send_corgi_photo.php
@@ -3,6 +3,6 @@ require 'snapchat.php';
 require 'reddit/corgi.php';
 
 $corgi = file_get_contents( Corgi::fetch_corgi() ) or die();
-SnapChatUtil::send_photo( $corgi, SnapchatUtil::get_friends_list() );
+SnapchatUtil::send_photo( $corgi, SnapchatUtil::get_friends_list() );
 
 exit( 0 );
